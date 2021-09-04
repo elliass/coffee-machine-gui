441565695af3e4d1d5cfeb4f300e5dda4d7026b8
Elliass Rachidi <rachidi.elliass@gmail.com>
2021-09-04 10:13:56 +0200

    Added states to CoffeeMachine class

--- src/main/java/coffeemachine/model/CoffeeMachine.java
+++ src/main/java/coffeemachine/model/CoffeeMachine.java
@@ -1,19 +1,24 @@
 package coffeemachine.model;
 
-import coffeemachine.controller.CoffeeMachineState;
-import coffeemachine.controller.IdleState;
+import coffeemachine.controller.*;
 
 
 public class CoffeeMachine {
 
     private CoffeeMachineState idleState;
     public CoffeeMachineState currentState;
+    private CoffeeMachineState preparationState;
+    private CoffeeMachineState maintenanceState;
+    private CoffeeMachineState issueState;
 
     private static CoffeeMachine instance = null;
 
     public CoffeeMachine() {
         idleState = new IdleState(this);
         this.currentState = idleState;
+        preparationState = new PreparationState(this);
+        maintenanceState = new MaintenanceState(this);
+        issueState = new IssueState(this);
     }
 
     public static CoffeeMachine getInstance(){
@@ -31,5 +36,14 @@ public class CoffeeMachine {
     }
     public CoffeeMachineState getIdleState(){
         return this.idleState;
+    }
+    public CoffeeMachineState getPreparationState(){
+        return this.preparationState;
+    }
+    public CoffeeMachineState getMaintenanceState(){
+        return this.maintenanceState;
+    }
+    public CoffeeMachineState getIssueState(){
+        return this.issueState;
     }
 }
