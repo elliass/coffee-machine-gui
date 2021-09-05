f475523f7858f44cb7ea5f89f8df51fc576d93aa
Elliass Rachidi <rachidi.elliass@gmail.com>
2021-09-05 15:57:03 +0200

    Added more events

--- src/main/java/coffeemachine/view/CustomEvent.java
+++ src/main/java/coffeemachine/view/CustomEvent.java
@@ -12,4 +12,18 @@ public class CustomEvent extends Event {
     public static final EventType<CustomEvent> GO_TO_OVERVIEW_SCENE =
             new EventType<>("GO_TO_OVERVIEW_SCENE");
 
+    public static final EventType<CustomEvent> GO_TO_PREPARATION_SCENE =
+            new EventType<>("GO_TO_PREPARATION_SCENE");
+
+    public static final EventType<CustomEvent> GO_TO_MAINTENANCE_SCENE =
+            new EventType<>("GO_TO_MAINTENANCE_SCENE");
+
+    public static final EventType<CustomEvent> GO_TO_ISSUE_SCENE =
+            new EventType<>("GO_TO_ISSUE_SCENE");
+
+    public static final EventType<CustomEvent> GO_TO_FAVORITE_SCENE =
+            new EventType<>("GO_TO_FAVORITE_SCENE");
+
+    public static final EventType<CustomEvent> GO_TO_SECURITY_SCENE =
+            new EventType<>("GO_TO_SECURITY_SCENE");
 }
